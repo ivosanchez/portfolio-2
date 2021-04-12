400e439d8e2f6707e4f20540f111e84296f6d35e
Jon Ganebski <jon.ganebski@gmail.com>
2021-04-12 23:16:55 +0900

    added active class and animation

--- src/components/AsideRight.vue
+++ src/components/AsideRight.vue
@@ -1,23 +1,46 @@
 <template>
   <aside class="aside-right">
-    <div class="aside-right__languages-container">
-      <span class="aside-right__lan" @click="useKorean">ko</span>
-      <span> / </span>
-      <span class="aside-right__lan" @click="useEnglish">en</span>
+    <div class="aside-right__langs-container" ref="langsContainerRef">
+      <span
+        :class="[language === 'ko' ? 'aside-right__lang--active' : 'aside-right__lang']"
+        @click="useKorean"
+        >ko</span
+      >
+      <span class="aside-right__lang-divider">/</span>
+      <span
+        :class="[language === 'en' ? 'aside-right__lang--active' : 'aside-right__lang']"
+        @click="useEnglish"
+        >en</span
+      >
     </div>
   </aside>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { DELAY_3_HALF } from '@/constants/gsap';
+import { GETTERS, useStore } from '@/store';
+import gsap from 'gsap';
+import { computed, defineComponent, onMounted, ref } from 'vue';
 import { useLanguage } from '../hooks/useLanguage';
 
 export default defineComponent({
   name: 'AsideRight',
   setup() {
+    const { getters } = useStore();
+    const language = computed(() => getters[GETTERS.GET_LANGUAGE]);
+    const langsContainerRef = ref<HTMLDivElement | null>(null);
     const fns = useLanguage();
 
-    return { ...fns };
+    onMounted(() => {
+      if (!langsContainerRef.value) return;
+      gsap.from(langsContainerRef.value, {
+        duration: 1,
+        delay: DELAY_3_HALF,
+        x: langsContainerRef.value.clientWidth,
+      });
+    });
+
+    return { ...fns, langsContainerRef, language };
   },
 });
 </script>
@@ -28,21 +51,28 @@ export default defineComponent({
   right: 0;
   padding-top: 2rem;
   padding-right: 1.5rem;
-  display: flex;
-  justify-content: flex-end;
-  .aside-right__languages-container {
+  .aside-right__langs-container {
     @media screen and (min-width: 1000px) {
-      display: block;
+      display: flex;
+      justify-content: flex-end;
     }
     display: none;
     color: white;
     font-size: 0.9rem;
-    .aside-right__lan {
+    overflow: hidden;
+    font-family: 'Poppins', sans-serif;
+    .aside-right__lang {
       pointer-events: auto;
       cursor: pointer;
       &:hover {
         color: $primary;
       }
+    }
+    .aside-right__lang--active {
+      color: $primary;
+    }
+    .aside-right__lang-divider {
+      margin: 0 5px;
     }
   }
 }
