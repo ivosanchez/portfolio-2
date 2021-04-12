33c61c26e015b3da79e4ac77e6e0ead6b682e988
Jon Ganebski <jon.ganebski@gmail.com>
2021-04-12 23:20:01 +0900

    refactoring onMounted

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,11 @@
   <div ref="scrollRef">
     <main>
       <div class="intro">
-        <div class="intro__top">
-          <div>
+        <div class="intro__top" ref="introTopRef">
+          <div class="intro__top__row">
             <span>Optimized for Google Chrome.</span>
           </div>
-          <div>
+          <div class="intro__top__row">
             <span>There are {{ count }} projects.</span>
             <span>Developed on WSL2.</span>
           </div>
@@ -44,6 +44,7 @@ import LeaveAnimation from '../components/LeaveAnimation.vue';
 import Project from '../components/Project.vue';
 import Title from '../components/Title.vue';
 import { IProject } from '../data/projects';
+import { DELAY_1, DELAY_2, DELAY_3_HALF } from '../constants/gsap';
 
 export default defineComponent({
   name: 'Home',
@@ -61,85 +62,71 @@ export default defineComponent({
   },
   setup(props) {
     const { getters } = useStore();
+    const introTopRef = ref<HTMLDivElement | null>(null);
     const h1Ref = ref<HTMLHeadingElement | null>(null);
     const arrDownRef = ref<HTMLSpanElement | null>(null);
     const language = computed(() => getters[GETTERS.GET_LANGUAGE]);
 
     const { scrollRef, ScrollTrigger } = useLocomitive();
 
+    const scrollTriggerRevealFromSide = (element: HTMLElement, index: number) => {
+      if (!scrollRef.value) return;
+      const animation = gsap.from(element, {
+        duration: 0.7,
+        x: index % 2 === 0 ? -element.clientWidth * 2 : element.clientWidth * 2,
+      });
+      ScrollTrigger.create({
+        trigger: element,
+        start: 'bottom 90%',
+        scroller: scrollRef.value,
+        animation,
+      });
+    };
+
+    const scrollTriggerRevealByOpacity = (element: HTMLElement) => {
+      if (!scrollRef.value) return;
+      const animation = gsap.from(element, {
+        duration: 0.7,
+        opacity: 0,
+        y: 20,
+      });
+      ScrollTrigger.create({
+        trigger: element,
+        scroller: scrollRef.value,
+        start: 'bottom 90%',
+        animation,
+      });
+    };
+
     onMounted(() => {
       if (!scrollRef.value) return;
-      const numbers = document.querySelectorAll('.project__number');
-      const names = document.querySelectorAll('.project__name');
-      const techs = document.querySelectorAll('.project__tech');
-      const descs = document.querySelectorAll('.project__desc');
+      const numbers = scrollRef.value.querySelectorAll<HTMLElement>('.project__number');
+      const names = scrollRef.value.querySelectorAll<HTMLElement>('.project__name');
 
-      numbers.forEach((number, index) => {
-        if (!scrollRef.value) return;
-        const animation = gsap.from(number, {
-          duration: 0.7,
-          x: index % 2 === 0 ? -number.clientWidth * 2 : number.clientWidth * 2,
-        });
-        ScrollTrigger.create({
-          trigger: number,
-          start: 'bottom 90%',
-          scroller: scrollRef.value,
-          animation,
-        });
-      });
+      const techs = scrollRef.value.querySelectorAll<HTMLElement>('.project__tech');
+      const descs = scrollRef.value.querySelectorAll<HTMLElement>('.project__desc');
 
-      names.forEach((name, index) => {
-        if (!scrollRef.value) return;
-        const animation = gsap.from(name, {
-          duration: 0.7,
-          x: index % 2 === 0 ? -name.clientWidth * 2 : name.clientWidth * 2,
-        });
-        ScrollTrigger.create({
-          trigger: name,
-          start: 'bottom 90%',
-          scroller: scrollRef.value,
-          animation,
-        });
-      });
+      numbers.forEach(scrollTriggerRevealFromSide);
+      names.forEach(scrollTriggerRevealFromSide);
 
-      techs.forEach((tech) => {
-        if (!scrollRef.value) return;
-        const animation = gsap.from(tech, {
-          duration: 0.7,
-          opacity: 0,
-          y: 20,
-        });
-        ScrollTrigger.create({
-          trigger: tech,
-          scroller: scrollRef.value,
-          start: 'bottom 90%',
-          animation,
-        });
-      });
+      techs.forEach(scrollTriggerRevealByOpacity);
+      descs.forEach(scrollTriggerRevealByOpacity);
 
-      descs.forEach((desc) => {
-        if (!scrollRef.value) return;
-        const animation = gsap.from(desc, {
-          duration: 0.7,
-          opacity: 0,
-          y: 20,
+      if (introTopRef.value) {
+        const spans = introTopRef.value.querySelectorAll('.intro__top__row');
+        spans.forEach((span) => {
+          gsap.from(span, { duration: 2, delay: DELAY_3_HALF, width: 0, whiteSpace: 'nowrap' });
         });
-        ScrollTrigger.create({
-          trigger: desc,
-          scroller: scrollRef.value,
-          start: 'top 90%',
-          animation,
-        });
-      });
+      }
 
       if (h1Ref.value) {
-        gsap.from(h1Ref.value, { duration: 1, delay: 1, x: -h1Ref.value.clientWidth - 5 });
+        gsap.from(h1Ref.value, { duration: 1, delay: DELAY_1, x: -h1Ref.value.clientWidth - 5 });
       }
 
       if (arrDownRef.value) {
         gsap.from(arrDownRef.value, {
           duration: 1,
-          delay: 2,
+          delay: DELAY_2,
           opacity: 0,
           y: -20,
         });
@@ -151,6 +138,7 @@ export default defineComponent({
 
     return {
       h1Ref,
+      introTopRef,
       arrDownRef,
       scrollRef,
       projects: props.PROJECTS,
@@ -180,13 +168,13 @@ main {
       display: grid;
       align-self: flex-start;
       grid-template-columns: 1fr 1fr;
-      span {
-        display: block;
-        color: white;
-        font-size: 0.7rem;
-      }
-      img {
-        width: 1rem;
+      .intro__top__row {
+        overflow: hidden;
+        span {
+          display: block;
+          color: white;
+          font-size: 0.7rem;
+        }
       }
     }
     h1 {
