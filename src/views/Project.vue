1b6ab95acaab7edd450883242f9562def292458f
Jon Ganebski <jon.ganebski@gmail.com>
2021-04-17 22:11:42 +0900

    project hero image adjustment

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -245,8 +245,9 @@ main {
     img {
       width: 100%;
       height: 100%;
+      min-height: 400px;
       object-fit: cover;
-      object-position: left;
+      object-position: center;
     }
   }
   .heading {
