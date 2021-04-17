fa9809536324e238410fb68240506e74877eb48a
Jon Ganebski <jon.ganebski@gmail.com>
2021-04-17 20:51:48 +0900

    intro improvement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,9 +12,41 @@
           <span>Developed on WSL2.</span>
         </div>
       </div>
-      <h1 ref="h1Ref">I make your ideas alive on the web</h1>
-      <div></div>
-      <div class="arrow-down" ref="arrDownRef">
+      <div class="intro__heading-container" ref="headingContainerRef">
+        <div>
+          <h1 class="intro__heading__secondary">I can help you</h1>
+        </div>
+        <div>
+          <h1 class="intro__heading__secondary">to cultivate</h1>
+        </div>
+        <h1>
+          <span class="intro__heading__primary">Solid </span>
+          <span class="intro__heading__primary">Web </span>
+          <span class="intro__heading__primary">Service</span>
+        </h1>
+      </div>
+      <div class="intro__desc" ref="descRef">
+        <div>
+          <span>{{
+            language === 'ko'
+              ? '안녕하세요! 저는 웹 개발자 방진석이에요.'
+              : "Hello! I'm web developer Jinseok Bang."
+          }}</span>
+        </div>
+        <div>
+          <span>{{
+            language === 'ko' ? '제 포트폴리오에 오신 걸 환영해요.' : 'Welcome to my portfolio.'
+          }}</span>
+        </div>
+        <div>
+          <span>{{
+            language === 'ko'
+              ? '스크롤해서 제가 뭘 할 수 있는지 봐주세요.'
+              : 'Scroll down and see what I can do.'
+          }}</span>
+        </div>
+      </div>
+      <div class="intro__arrow-down" ref="arrDownRef">
         <span>&darr;</span>
       </div>
     </div>
@@ -41,12 +73,12 @@ import useLocomitive from '@/hooks/useLocomotive.vue';
 import { GETTERS, useStore } from '@/store';
 import gsap from 'gsap';
 import { computed, defineComponent, onMounted, PropType, ref } from 'vue';
+import CustomCursor from '../components/CustomCursor.vue';
 import Footer from '../components/Footer.vue';
 import LeaveAnimation from '../components/LeaveAnimation.vue';
 import Project from '../components/Project.vue';
 import Title from '../components/Title.vue';
-import CustomCursor from '../components/CustomCursor.vue';
-import { DELAY_1, DELAY_2, DELAY_3_HALF } from '../constants/gsap';
+import { DELAY_3_HALF } from '../constants/gsap';
 import { IProject } from '../data/projects';
 
 export default defineComponent({
@@ -67,7 +99,8 @@ export default defineComponent({
   setup(props) {
     const { getters } = useStore();
     const introTopRef = ref<HTMLDivElement | null>(null);
-    const h1Ref = ref<HTMLHeadingElement | null>(null);
+    const headingContainerRef = ref<HTMLHeadingElement | null>(null);
+    const descRef = ref<HTMLDivElement | null>(null);
     const arrDownRef = ref<HTMLSpanElement | null>(null);
     const language = computed(() => getters[GETTERS.GET_LANGUAGE]);
 
@@ -119,29 +152,55 @@ export default defineComponent({
       if (introTopRef.value) {
         const spans = introTopRef.value.querySelectorAll('.intro__top__row');
         spans.forEach((span) => {
-          gsap.from(span, { duration: 2, delay: DELAY_3_HALF, width: 0, whiteSpace: 'nowrap' });
+          gsap.from(span, {
+            duration: 1,
+            delay: DELAY_3_HALF,
+            clipPath: 'inset(0% 0% 0% 100%)',
+          });
         });
       }
 
-      if (h1Ref.value) {
-        gsap.from(h1Ref.value, { duration: 1, delay: DELAY_1, x: -h1Ref.value.clientWidth - 5 });
+      if (headingContainerRef.value) {
+        const secondaries = headingContainerRef.value.querySelectorAll<HTMLHeadingElement>(
+          '.intro__heading__secondary'
+        );
+        const primaryParts = headingContainerRef.value.querySelectorAll<HTMLSpanElement>(
+          '.intro__heading__primary'
+        );
+        const tl = gsap.timeline({ defaults: { duration: 1 } });
+        tl.from(secondaries, {
+          clipPath: 'inset(0% 100% 0% 0%)',
+          stagger: 0.5,
+        });
+        tl.from(primaryParts, { duration: 1.2, clipPath: 'inset(100% 0% 0% 0%)' }, '+=0.5');
+      }
+
+      if (descRef.value) {
+        const descParts = descRef.value.querySelectorAll('span');
+        gsap.from(descParts, {
+          duration: 1,
+          delay: DELAY_3_HALF,
+          clipPath: 'inset(0% 100% 0% 0%)',
+        });
       }
 
       if (arrDownRef.value) {
         gsap.from(arrDownRef.value, {
           duration: 1,
-          delay: DELAY_2,
+          delay: DELAY_3_HALF,
           opacity: 0,
         });
       }
+
       setTimeout(() => {
         ScrollTrigger.refresh(true);
       }, 1000);
     });
 
     return {
-      h1Ref,
+      headingContainerRef,
       introTopRef,
+      descRef,
       arrDownRef,
       scrollRef,
       projects: props.PROJECTS,
@@ -157,22 +216,16 @@ main {
   position: relative;
   padding-bottom: 10vh;
   .intro {
-    @include mobile-23-desktop-2345__margins;
     height: 100vh;
     display: grid;
     grid-template-rows: repeat(4, 1fr);
-    align-items: center;
     overflow: hidden;
     .intro__top {
-      @media screen and (min-width: 1024px) {
-        padding-left: $column-line-2-left;
-      }
+      @include mobile-23-desktop-45__margins;
       padding-top: 2rem;
       display: grid;
-      align-self: flex-start;
       grid-template-columns: 1fr 1fr;
       .intro__top__row {
-        overflow: hidden;
         span {
           display: block;
           color: white;
@@ -180,21 +233,40 @@ main {
         }
       }
     }
-    h1 {
-      @media screen and (min-width: 600px) {
-        font-size: 4rem;
+    .intro__heading-container {
+      @include mobile-23-desktop-2345__paddings;
+      display: flex;
+      flex-direction: column;
+      .intro__heading__secondary {
+        display: inline;
+        font-size: $font-size-m;
+        line-height: 1.4em;
+        font-weight: 400;
+        box-sizing: content-box;
+        color: white;
       }
-      @media screen and (min-width: 1024px) {
-        font-size: 6rem;
+      .intro__heading__primary {
+        margin-top: 1rem;
+        line-height: 1em;
+        color: $primary;
+        font-size: $font-size-xl;
+        font-weight: 600;
       }
-      display: inline-block;
-      color: $primary;
-      font-size: 2rem;
-      font-weight: 600;
-      box-sizing: content-box;
     }
-    .arrow-down {
+    .intro__desc {
+      @include mobile-23-desktop-45__paddings;
+      align-self: center;
+      span {
+        display: inline-block;
+        color: white;
+        font-size: $font-size-xs;
+        line-height: 1.2em;
+        font-weight: 300;
+      }
+    }
+    .intro__arrow-down {
       width: 100%;
+      align-self: center;
       display: flex;
       justify-content: center;
       color: white;
